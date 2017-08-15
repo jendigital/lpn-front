b1ccf87daf6e40f525da84332ec0692f3e37e968
Jennifer Lim <contact@jendigitalvision.com>
2017-08-15 11:40:30 +0200

    Admin 01(feat): Add http-auth for heroku preproduction

--- server/index.js
+++ server/index.js
@@ -4,20 +4,31 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const auth = require('http-auth');
+const basic = auth.basic({
+    realm: "Life Personal Network Area.",
+	  file: __dirname + "/.htpasswd"
+});
+
+app.use(auth.connect(basic));
+app.get('/', (req, res) => {
+    res.send(`Hello from express - ${req.user}!`);
+});
+
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Answer API requests.
 app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
 });
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
