7c2b53169ee8906024a0a537e0bc39ec4cb86749
Jennifer Lim <contact@jendigitalvision.com>
2017-09-30 12:40:37 +0200

    US-00(bugfix): fix signin error

--- react-ui/src/app/pages/authentification/modal/subscription_error/index.js
+++ react-ui/src/app/pages/authentification/modal/subscription_error/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { FormattedMessage } from 'react-intl'
 import $ from 'jquery'
 
+import './index.css'
 import '../modal.css'
 
 class SigninError extends Component {
@@ -20,48 +21,48 @@ class SigninError extends Component {
         return (
             <div id="signin_error">
                 <div className="toaster info" id="email_match" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.email" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.email" />
                 </div>
                 <div className="toaster error" id="pwd_match" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.pwd" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.pwd" />
                 </div>
                 <div className="toaster warning" id="pwd_confirm" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.confirm" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.confirm" />
                 </div>
                 <div className="toaster info" id="lastname_match" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.lastname" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.lastname" />
                 </div>
                 <div className="toaster info" id="firstname_match" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.firstname" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.firstname" />
                 </div>
                 <div className="toaster info" id="gender_select" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.gender" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.gender" />
                 </div>
                 <div className="toaster warning" id="date_select" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.date" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.date" />
                 </div>
                 <div className="toaster warning" id="legal_date" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.legaldate" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.legaldate" />
                 </div>
                 <div className="toaster info" id="country_select" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.country" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.country" />
                 </div>
                 <div className="toaster info" id="city_match" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.city" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.city" />
                 </div>
                 <div className="toaster error" id="fill_field" onClick={this.offAlerts} >
-                    <FormattedMessage id="signin_err.form" />
                     <span className="glyphicon glyphicon-remove" />
+                    <FormattedMessage id="signin_err.form" />
                 </div>
             </div>
         )
